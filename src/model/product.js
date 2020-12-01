53eb97928a649959606873e00b2353709aea10e9
amardito <amardito37@gmail.com>
2020-12-01 13:00:33 +0700

    adding field on api get product and search

--- src/model/product.js
+++ src/model/product.js
@@ -12,7 +12,7 @@ const createProduct = (payload) => new Promise((resolve, reject) => {
 });
 
 const getProduct = (payload) => new Promise((resolve, reject) => {
-  const qStr = 'SELECT p.id_product, p.product_name, p.product_by, p.product_price, p.product_qty, c.category_name, p.product_desc, p.created_at, p.updated_at FROM products AS p JOIN category AS c ON c.id_category = p.category_id WHERE p.id_product = ?';
+  const qStr = 'SELECT p.id_product, p.product_name, p.product_by, p.product_price, p.product_qty, c.category_name, p.product_desc, p.product_sold, p.created_at, p.updated_at FROM products AS p JOIN category AS c ON c.id_category = p.category_id WHERE p.id_product = ?';
   db.query(qStr, payload, (err, data) => {
     if (!err) {
       resolve(data);

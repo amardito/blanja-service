7b295c1b4836dae9433b353e7bb18b2a14ede86b
amardito <amardito37@gmail.com>
2020-12-13 16:07:21 +0700

    modeling response status to header

--- src/helper/wrapper.js
+++ src/helper/wrapper.js
@@ -1,14 +1,12 @@
 module.exports = {
   success: (res, message, data, status) => {
     res.status(status).json({
-      status,
       message,
       data,
     });
   },
   error: (res, message, error, status) => {
     res.status(status).json({
-      status,
       message,
       error,
     });
